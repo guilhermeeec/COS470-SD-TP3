c0a4466e553492f9989471a74fb0641312f0a52c
guilhermeeec <guilhermepic2016@gmail.com>
2023-06-21 14:01:15 -0300

    Aprendendo a biblioteca de RPC

---
+++ example_server_frontend.cpp
@@ -0,0 +1,27 @@
+// g++ -std=c++11 -o example_server_frontend example_server_frontend.cpp -lrpc -lpthread
+
+#include <iostream>
+#include "rpc/server.h"
+
+void foo() {
+    std::cout << "foo was called!" << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+    // Creating a server that listens on port 8080
+    rpc::server srv(8080);
+
+    // Binding the name "foo" to free function foo.
+    // note: the signature is automatically captured
+    srv.bind("foo", &foo);
+
+    // Binding a lambda function to the name "add".
+    srv.bind("add", [](int a, int b) {
+        return a + b;
+    });
+
+    // Run the server loop.
+    srv.run();
+
+    return 0;
+}
