828bc02d45a07cc6d43bc46dca709397ddfa6ad3
guilhermedionisio <gsantanadionisio@gmail.com>
2023-07-15 13:56:29 -0300

    Test1 validados corretamente

---
+++ test1.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Valores de n a serem testados
+n_values=(2 4 8 16 32)
+
+# Fixar r e k
+r=10
+k=2
+
+# Loop através dos valores de n
+for n in "${n_values[@]}"
+do
+    # Executa o coordinator em segundo plano
+    ./coordinator 4 $n $r &
+
+    # Armazena o PID do coordinator
+    coordinator_pid=$!
+
+    sleep 5
+
+    # Executa os processos em segundo plano
+    for ((i=0; i<n; i++))
+    do
+        ./process $i 10 $k &
+    done
+
+    # Aguarda o coordinator terminar de executar
+    wait $coordinator_pid
+
+    # Executa o validador de resultado e armazena a saída em uma variável
+    resultado_output=$(python3 resultado_validator.py "resultado.txt" $n $r "test1_time_execution.txt")
+
+    # Verifica se a saída do validador de resultado contém a mensagem esperada
+    if [[ $resultado_output == "The result is valid." ]]; then
+        # Executa o validador de log e armazena a saída em uma variável
+        log_output=$(python3 log_validator.py)
+        
+        # Verifica se a saída do validador de log contém a mensagem esperada
+        if [[ $log_output == "Log file was successfully validated" ]]; then
+            echo "Test 1 - n=$n: All validations done successfully."
+        else
+            echo "Test 1 - n=$n: Error: Log validation failed."
+        fi
+    else
+        echo "Test 1 - n=$n: Error: Result validation failed."
+    fi
+
+    # Limpa os arquivos de resultado e log para o próximo teste
+    rm resultado.txt
+    rm log.txt
+
+    sleep 5
+done
